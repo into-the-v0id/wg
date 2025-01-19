8867beeb8fb35b4a31ac3e65acad31785b7d32a7
Oliver Amann <me+git@oliver-amann.dev>
2025-01-19 11:16:27 +0100

    wip

--- src/application/chore.rs
+++ src/application/chore.rs
@@ -10,6 +10,29 @@ use crate::domain::user;
 use super::authentication::AuthSession;
 
 #[derive(Template)]
+#[template(path = "page/chore_list/chore/list.jinja")]
+struct ListTemplate {
+    chore_list: chore_list::ChoreList,
+    chores: Vec<chore::Chore>,
+}
+
+pub async fn view_list(
+    Path(id): Path<Uuid>,
+    State(state): State<Arc<AppState>>,
+    _auth_session: AuthSession,
+) -> Result<Html<String>, StatusCode> {
+    let chore_list = match chore_list::get_by_id(&state.pool, &id).await {
+        Ok(chore_list) => chore_list,
+        Err(sqlx::Error::RowNotFound) => return Err(StatusCode::NOT_FOUND),
+        Err(err) => panic!("{}", err),
+    };
+
+    let chores = chore::get_all_for_chore_list(&state.pool, &chore_list.id).await.unwrap();
+
+    Ok(Html(ListTemplate {chore_list, chores}.render().unwrap()))
+}
+
+#[derive(Template)]
 #[template(path = "page/chore_list/chore/detail.jinja")]
 struct DetailTemplate {
     chore: chore::Chore,
@@ -33,6 +56,65 @@ pub async fn view_detail(
     let chore_list = chore_list::get_by_id(&state.pool, &chore.chore_list_id).await.unwrap();
 
     Ok(Html(DetailTemplate {chore, chore_list}.render().unwrap()))
+}
+
+#[derive(Template)]
+#[template(path = "page/chore_list/chore/create.jinja")]
+struct CreateTemplate {
+    chore_list: chore_list::ChoreList,
+}
+
+pub async fn view_create_form(
+    Path(chore_list_id): Path<Uuid>,
+    State(state): State<Arc<AppState>>,
+    _auth_session: AuthSession,
+) -> Result<Html<String>, StatusCode> {
+    let chore_list = match chore_list::get_by_id(&state.pool, &chore_list_id).await {
+        Ok(chore_list) => chore_list,
+        Err(sqlx::Error::RowNotFound) => return Err(StatusCode::NOT_FOUND),
+        Err(err) => panic!("{}", err),
+    };
+    if chore_list.is_deleted() {
+        return Err(StatusCode::FORBIDDEN);
+    }
+
+    Ok(Html(CreateTemplate {chore_list}.render().unwrap()))
+}
+
+#[derive(serde::Deserialize, Debug)]
+#[allow(dead_code)]
+pub struct CreatePayload {
+    name: String,
+    points: i32,
+}
+
+pub async fn create(
+    Path(chore_list_id): Path<Uuid>,
+    State(state): State<Arc<AppState>>,
+    _auth_session: AuthSession,
+    Form(payload): Form<CreatePayload>,
+) -> Result<Redirect, StatusCode> {
+    let chore_list = match chore_list::get_by_id(&state.pool, &chore_list_id).await {
+        Ok(chore_list) => chore_list,
+        Err(sqlx::Error::RowNotFound) => return Err(StatusCode::NOT_FOUND),
+        Err(err) => panic!("{}", err),
+    };
+    if chore_list.is_deleted() {
+        return Err(StatusCode::FORBIDDEN);
+    }
+
+    let chore = chore::Chore {
+        id: Uuid::now_v7(),
+        chore_list_id: chore_list.id,
+        name: payload.name,
+        points: payload.points,
+        date_created: chrono::offset::Utc::now(),
+        date_deleted: None,
+    };
+
+    chore::create(&state.pool, &chore).await.unwrap();
+
+    Ok(Redirect::to(&format!("/chore-lists/{}/chores/{}", chore_list.id, chore.id)))
 }
 
 #[derive(Template)]
--- src/application/chore_activity.rs
+++ src/application/chore_activity.rs
@@ -10,6 +10,33 @@ use crate::domain::user;
 use super::authentication::AuthSession;
 
 #[derive(Template)]
+#[template(path = "page/chore_list/activity/list.jinja")]
+struct ListTemplate {
+    chore_list: chore_list::ChoreList,
+    activities: Vec<chore_activity::ChoreActivity>,
+    chores: Vec<chore::Chore>,
+    users: Vec<user::User>,
+}
+
+pub async fn view_list(
+    Path(id): Path<Uuid>,
+    State(state): State<Arc<AppState>>,
+    _auth_session: AuthSession,
+) -> Result<Html<String>, StatusCode> {
+    let chore_list = match chore_list::get_by_id(&state.pool, &id).await {
+        Ok(chore_list) => chore_list,
+        Err(sqlx::Error::RowNotFound) => return Err(StatusCode::NOT_FOUND),
+        Err(err) => panic!("{}", err),
+    };
+
+    let chores = chore::get_all_for_chore_list(&state.pool, &chore_list.id).await.unwrap();
+    let activities = chore_activity::get_all_for_chore_list(&state.pool, &chore_list.id).await.unwrap();
+    let users = user::get_all(&state.pool).await.unwrap();
+
+    Ok(Html(ListTemplate {chore_list, activities, chores, users}.render().unwrap()))
+}
+
+#[derive(Template)]
 #[template(path = "page/chore_list/activity/detail.jinja")]
 struct DetailTemplate {
     activity: chore_activity::ChoreActivity,
@@ -39,6 +66,80 @@ pub async fn view_detail(
     let user = user::get_by_id(&state.pool, &activity.user_id).await.unwrap();
 
     Ok(Html(DetailTemplate {activity, chore, chore_list, user, auth_session}.render().unwrap()))
+}
+
+#[derive(Template)]
+#[template(path = "page/chore_list/activity/create.jinja")]
+struct CreateTemplate {
+    chore_list: chore_list::ChoreList,
+    chores: Vec<chore::Chore>,
+    now: chrono::DateTime<chrono::Utc>
+}
+
+pub async fn view_create_form(
+    Path(chore_list_id): Path<Uuid>,
+    State(state): State<Arc<AppState>>,
+    _auth_session: AuthSession,
+) -> Result<Html<String>, StatusCode> {
+    let chore_list = match chore_list::get_by_id(&state.pool, &chore_list_id).await {
+        Ok(chore_list) => chore_list,
+        Err(sqlx::Error::RowNotFound) => return Err(StatusCode::NOT_FOUND),
+        Err(err) => panic!("{}", err),
+    };
+    if chore_list.is_deleted() {
+        return Err(StatusCode::FORBIDDEN);
+    }
+
+    let chores = chore::get_all_for_chore_list(&state.pool, &chore_list.id).await.unwrap();
+    let now = chrono::offset::Utc::now();
+
+    Ok(Html(CreateTemplate {chore_list, chores, now}.render().unwrap()))
+}
+
+#[derive(serde::Deserialize, Debug)]
+#[allow(dead_code)]
+pub struct CreatePayload {
+    chore_id: Uuid,
+    date: chrono::NaiveDate,
+}
+
+pub async fn create(
+    Path(chore_list_id): Path<Uuid>,
+    State(state): State<Arc<AppState>>,
+    auth_session: AuthSession,
+    Form(payload): Form<CreatePayload>,
+) -> Result<Redirect, StatusCode> {
+    let chore_list = match chore_list::get_by_id(&state.pool, &chore_list_id).await {
+        Ok(chore_list) => chore_list,
+        Err(sqlx::Error::RowNotFound) => return Err(StatusCode::NOT_FOUND),
+        Err(err) => panic!("{}", err),
+    };
+    if chore_list.is_deleted() {
+        return Err(StatusCode::FORBIDDEN);
+    }
+
+    let chore = match chore::get_by_id(&state.pool, &payload.chore_id).await {
+        Ok(chore) => chore,
+        Err(sqlx::Error::RowNotFound) => return Err(StatusCode::UNPROCESSABLE_ENTITY),
+        Err(err) => panic!("{}", err),
+    };
+
+    if chore.chore_list_id != chore_list.id {
+        return Err(StatusCode::UNPROCESSABLE_ENTITY);
+    }
+
+    let activity = chore_activity::ChoreActivity {
+        id: Uuid::now_v7(),
+        chore_id: chore.id,
+        user_id: auth_session.user_id,
+        date: payload.date,
+        date_created: chrono::offset::Utc::now(),
+        date_deleted: None,
+    };
+
+    chore_activity::create(&state.pool, &activity).await.unwrap();
+
+    Ok(Redirect::to(&format!("/chore-lists/{}/activities", chore_list.id)))
 }
 
 #[derive(Template)]
