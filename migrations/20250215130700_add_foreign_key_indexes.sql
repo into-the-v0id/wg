112212ea689d6b8a21176749b227746d1698c569
Oliver Amann <me+git@oliver-amann.dev>
2025-02-15 14:47:59 +0100

    Add several DB indexes for foreign keys

---
+++ migrations/20250215130700_add_foreign_key_indexes.sql
@@ -0,0 +1,3 @@
+create index chores_chore_list_id_idx on chores(chore_list_id);
+create index chore_activities_chore_id_idx on chore_activities(chore_id);
+create index chore_activities_user_id_idx on chore_activities(user_id);
