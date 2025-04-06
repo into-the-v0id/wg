a10f1b796991e8b739c45bc6d1d2605f8071bc41
Oliver Amann <me+git@oliver-amann.dev>
2025-04-06 12:25:22 +0200

    Use DB connection ping instead of a dummy select for health check

--- src/application/health.rs
+++ src/application/health.rs
@@ -1,13 +1,10 @@
 use std::sync::Arc;
 use axum::{extract::State, http::StatusCode};
+use sqlx::Connection;
 use crate::AppState;
 
 pub async fn check(State(state): State<Arc<AppState>>) -> StatusCode {
-    let result = match sqlx::query_as::<_, (i32,)>("SELECT 1 + 1").fetch_one(&state.pool).await {
-        Ok(result) => result.0,
-        Err(_) => return StatusCode::INTERNAL_SERVER_ERROR,
-    };
-    if result != 2 {
+    if state.pool.acquire().await.unwrap().as_mut().ping().await.is_err() {
         return StatusCode::INTERNAL_SERVER_ERROR;
     }
 
